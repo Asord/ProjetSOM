0798aead15b8e6ac387649e93e9da10b7ebeb888
Sophie RAUDRANT <sophie.raudrant@gmail.com>
2017-11-29 14:31:14 +0100

    Ajout de Vector.h

---
+++ Projet/SOM/Vector.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Utility.h"
+
+namespace SOM
+{
+	struct Vector
+	{
+		uint* pos;
+		uint dim;
+		
+		// Constructeur par défaut
+		Vector()
+		{
+			pos = new uint[2];
+			dim = 2;
+		}
+
+		// Constructeur
+		Vector(uint dim)
+		{
+			pos = new uint[dim];
+			dim = dim;
+		}
+
+		~Vector() { delete[] pos; }
+
+		uint& operator[](uint dim) { return pos[dim]; }
+		const uint& operator[](uint dim) const { return pos[dim]; }
+		uint& operator()(uint dim) { return pos[dim]; }
+		const uint& operator()(uint dim) const { return pos[dim]; }
+	};
+}
+
