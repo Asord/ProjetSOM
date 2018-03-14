e9345971d7227ba47b999b810123ffe71d0c9e89
Sophie RAUDRANT <sophie.raudrant@gmail.com>
2018-03-14 12:26:55 +0100

    Modification de la lecture des fichiers

--- Projet/PTUT/Resources.cpp
+++ Projet/PTUT/Resources.cpp
@@ -1,104 +1,39 @@
 #include "Resources.h"
 #include <algorithm>
 
-#define randRange(min, max) rand() % (max - min) + min + 1
-
-namespace SOM // SomElement
-{
-	SomElement::SomElement(uint dimension)
-	{
-		this->dimension = dimension;
-		
-		for (uint i = 0; i < dimension; ++i)
-			this->vector.push_back((float)rand());
-	}
-
-	SomElement::SomElement(uint dimension, int min, int max)
-	{
-		this->dimension = dimension;
-
-		for (uint i = 0; i < dimension; ++i)
-			this->vector.push_back((float)(randRange(min, max)));
-	}
-
-	SomElement::SomElement(std::vector<float>& data, uint dimension)
-	{
-		this->dimension = dimension;
-
-		for (uint i = 0; i < dimension; ++i)
-			this->vector.push_back(data[i]);
-	}
-
-	float& SomElement::operator[](uint dim)
-	{
-		return this->vector[dim];
-	}
-
-}
-
 namespace SOM // Resources
 {
-	Resources::Resources(uint height, uint width)
-	{
-		this->height = height;
-		this->width  = width;
-		this->size   = height * width;
+	//Resources::Resources(uint size)
+	//{
+	//	this->size = size;
 
-		srand((uint)time(NULL));
+	//	int sqrt_size = isqrt(size);
+	//	if (sqrt_size * sqrt_size != size)
+	//	{
+	//		std::cerr << "La taille des ressources assignées à sa construction n'est pas un carré.";
+	//		exit(EXIT_FAILURE);
+	//	}
 
-		for (uint i = 0; i < size; ++i)
-			vector.push_back(SomElement(3));
+	//	srand((uint)time(NULL));
 
-	}
+	//	for (uint i = 0; i < size; ++i)
+	//		vector.push_back(SomElement(3));
 
-	Resources::Resources(uint size)
-	{
-		this->size = size;
-
-		int sqrt_size = isqrt(size);
-		if (sqrt_size * sqrt_size != size)
-		{
-			std::cerr << "La taille des ressources assignées à sa construction n'est pas un carré.";
-			exit(EXIT_FAILURE);
-		}
-
-		srand((uint)time(NULL));
-
-		for (uint i = 0; i < size; ++i)
-			vector.push_back(SomElement(3));
-
-	}
+	//}
 
 	Resources::Resources(std::string filePath)
 	{
-		m_fichier = fopen(filePath.std::string::c_str(), "rb");
+		image = QImage(QString::fromStdString(filePath));
+		
+		// TODO: handle not found issue
 
-		if (m_fichier == NULL) 
-		{
-			std::cerr << "Impossible d'ouvrir le fichier en lecture !";
-			exit(EXIT_FAILURE);
-		}
+		uchar* ptr = image.bits();
 
-		bitmapHeader header;
-		header.readBitmapHeader(m_fichier);
+		QSize qsize = image.size();
 
-		this->size = header.nbPix;
-		this->height = header.height;
-		this->width = header.width;
+		this->size = qsize.height() * qsize.width() * image.pixelFormat().bitsPerPixel();
+		this->height = qsize.height();
+		this->width = qsize.width();
 
-		uchar byte_buffer;
-		SomElement color_buffer(3);
-
-		for (uint i = 0; i < size; ++i)
-		{
-			for (uint col = 0; col < 3; ++col)
-			{
-				fread(&byte_buffer, sizeof(uchar), 1, m_fichier);
-				color_buffer[col] = byte_buffer;
-			}
-			vector.push_back(color_buffer);
-		}
-
-		fclose(m_fichier);
 	}
 }
--- Projet/PTUT/Resources.h
+++ Projet/PTUT/Resources.h
@@ -5,37 +5,18 @@
 #include <time.h>
 #include <iostream>
 #include <vector>
+#include <qimage.h>
 
 namespace SOM
 {
-	static const double fColorMinAct = sqrt(3 * pow(2, 16 * sizeof(uchar)));
-
-	class SomElement
-	{
-	protected:
-		uchar dimension;
-		std::vector<float> vector;
-
-	public:
-		SomElement(uint dimension);
-		SomElement(uint dimension, int min, int max);
-		SomElement(std::vector<float>& data, uint dimension);
-
-		float& operator[](uint dim);
-	};
-
 	class Resources
 	{
-	private:
-
-		FILE* m_fichier;
-
-
 	public:
-		std::vector<SomElement> vector;
+		//std::vector<QImage> vector;
 		uint size;
 		uint height;
 		uint width;
+		QImage image;
 
 		Resources(uint size);
 		Resources(uint height, uint width);
